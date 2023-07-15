36450f16eb5854c32a47c8f9599e7921d376da9d
Djuri Schiffer <djuri_schiffer@hotmail.com>
2023-07-15 22:21:53 +0200

    Added correct tailwind and cleaned up code

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { fetchData } from "@/lib/getDataFromGql";
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 import { HOME_DATA } from "@/lib/fragments";
 import Banner from "./_components/Banners";
 
@@ -7,29 +7,28 @@ export default async function Home() {
   async function getPageDataByUri() {
     const data = await fetchData(
       gql`
-      ${HOME_DATA}
-      query GetNodeByUri($uri: String!) {
+        ${HOME_DATA}
+        query GetNodeByUri($uri: String!) {
           nodeByUri(uri: $uri) {
-              __typename
-              ...HomeData
+            __typename
+            ...HomeData
           }
-      }
-    `,
-      { uri: '/'}
+        }
+      `,
+      { uri: "/" }
     );
 
     return data;
   }
 
   const { data } = await getPageDataByUri();
-  console.log(data);
   const pageData = data?.nodeByUri?.homeData;
 
-  console.log('pageData', pageData);
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-        {pageData?.homeBanner && <Banner id="home" componentData={pageData?.homeBanner} />}
-    </main>
-  )
+    <>
+      {pageData?.homeBanner && (
+        <Banner id="home" componentData={pageData?.homeBanner} />
+      )}
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,48 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./node_modules/flowbite-react/**/*.js", "./app/**/*.{ts,tsx}"],
   theme: {
+    colors: {
+      emerald: colors.emerald,
+      white: "#ffffff",
+      black: "#000000",
+      loss: "#ea3943",
+      dark: "#1e293b",
+      "gray-dark": "#1e2126",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        green: {
+          // primary
+          50: "#ecfdf5",
+          100: "#d1fae5",
+          200: "#a7f3d0",
+          300: "#6ee7b7",
+          400: "#34d399",
+          500: "#10b981",
+          600: "#10b981",
+          700: "#10b981",
+          800: "#10b981",
+          900: "#10b981",
+          DEFAULT: "#34d399", //500
+          on: "#ecfdf5", //50
+          dark: {
+            DEFAULT: "#34d399", //400
+            on: "#ecfdf5", //800
+          },
+        },
+      },
+      extend: {
+        fontSize: {
+          "8xl": "6rem",
+        },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+  darkMode: "class",
+};
